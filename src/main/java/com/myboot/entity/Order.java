d572b559360ca3de36d389ae416f5c27bebf3203
alastril <alastril@i.ua>
2023-03-01 17:56:58 +0200

    add kafka dependency + test entity

---
+++ src/main/java/com/myboot/entity/Order.java
@@ -0,0 +1,16 @@
+package com.myboot.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    long id;
+    @JsonProperty("name")
+    String name;
+}
