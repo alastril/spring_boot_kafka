1107e92451b4d6beca92466c15d7f44fe1d13b07
alastril <alastril@i.ua>
2024-11-10 14:34:19 +0200

    fix test

--- src/test/java/com/myboot/KafkaIntegrationTest.java
+++ src/test/java/com/myboot/KafkaIntegrationTest.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.ArgumentCaptor;
@@ -18,8 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.Lifecycle;
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -95,7 +92,7 @@ public class KafkaIntegrationTest {
 
     @Test
    public void checkListenerZeroToOnePartitions() throws Exception {
-        MessageSimple message = new MessageSimple(1L, "order");
+        MessageSimple message = new MessageSimple(1L, "order1");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/kafka/send")
                 .contentType("application/json")
@@ -112,8 +109,8 @@ public class KafkaIntegrationTest {
     @Test
     public void checkBatchListenerWithReply() throws Exception {
         List<MessageSimple> messages = new ArrayList<>();
-        messages.add(new MessageSimple(1L, "order"));
-        messages.add(new MessageSimple(2L, "order2"));
+        messages.add(new MessageSimple(1L, "2order1"));
+        messages.add(new MessageSimple(2L, "2order2"));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/kafka/sendToBatch")
                 .contentType("application/json")
@@ -124,6 +121,7 @@ public class KafkaIntegrationTest {
         Assert.isTrue(messageBatchArgumentCaptor.getValue().stream().findFirst().get().getPayload().size() == 2, "Message list must be 2");
         Awaitility.await().atMost(Duration.ofSeconds(1)).untilAsserted(
                 () -> Mockito.verify(consumer, Mockito.atLeast(1)).listenReplyRead(stringArgumentCaptor.capture()));
-        Assert.isTrue(objectMapper.readValue(stringArgumentCaptor.getValue().toString(), List.class).size() >= 2, "Size must be 2");
+        Awaitility.await().atMost(Duration.ofSeconds(1)).untilAsserted(
+                () -> Assert.isTrue(messageBatchArgumentCaptor.getValue().stream().findFirst().get().getPayload().size() == 2, "Message list must be 2"));
     }
 }
